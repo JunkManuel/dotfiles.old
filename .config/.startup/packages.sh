22b18b2e7ca1c33a6b5e2d3998696bd51c807658
JunkManuel <ramavillasenin@gmail.com>
2023-07-23 21:33:14 +0000

    startup changes

--- .config/.startup/packages.sh
+++ .config/.startup/packages.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/sh
 
-packages=("zsh" "git" "firefox" "neovim" "picom" "polybar" "gcc" "python" "code" "alacritty" "rofi" "xournalpp" "networkmanager" "network-manager-applet" "jq" "udisks2" "ntfs-3g" "openssh" "syncthing" "keepassxc")
+# Yay (yet another yaourt) installation
+if ! builtin type -p 'yay' >/dev/null 2>&1; then
+    echo 'Install yay.'
+    sudo pacman -S --needed base base-devel wget
+    tmpdir="$(command mktemp -d)"
+    command cd "${tmpdir}" || return 1
+    dl_url="$(command curl -sfLS 'https://api.github.com/repos/Jguer/yay/releases/latest' |
+        command grep 'browser_download_url' |
+        command tail -1 |
+        command cut -d '"' -f 4
+    )"
+    command wget "${dl_url}"
+    command tar xzvf yay_*_x86_64.tar.gz
+    command cd yay_*_x86_64 || return 1
+    ./yay -Sy yay-bin
+    rm -rf "${tmpdir}"
+fi
+
+packages=("git" "zsh" "zsh-completions" "zsh-syntax-highlighting" "firefox" "neovim" "picom" "polybar" "gcc" "python" "code" "alacritty" "rofi" "xournalpp" "networkmanager" "network-manager-applet" "jq" "udisks2" "ntfs-3g" "openssh" "syncthing" "keepassxc" "libreoffice-still")
 
 pacman -Sy
 pacman -S ${packages[@]}
+
+echo "\nInstall onedrive-abraunegg through yay"
