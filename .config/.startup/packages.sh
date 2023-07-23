886cad08672058ab5ca2aa61bdef86e1832b0ec9
KiraManolo <ramavillasenin@gmail.com>
2023-07-23 18:02:36 +0200

    Update packages.sh

--- .config/.startup/packages.sh
+++ .config/.startup/packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-packages=("git" "firefox" "neovim" "picom" "polybar" "gcc" "python" "code" "alacritty" "rofi" "xournalpp" "networkmanager" "network-manager-applet" "jq" "udisks2" "ntfs-3g" "openssh" "syncthing" "keepassxc")
+packages=("zsh" "git" "firefox" "neovim" "picom" "polybar" "gcc" "python" "code" "alacritty" "rofi" "xournalpp" "networkmanager" "network-manager-applet" "jq" "udisks2" "ntfs-3g" "openssh" "syncthing" "keepassxc")
 
 pacman -Sy
 pacman -S ${packages[@]}
