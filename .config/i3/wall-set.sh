9f7127e68c2a8001fd3eaf3daba341992f113c56
JunkManuel <ramavillasenin@gmail.com>
2023-11-14 21:18:21 +0100

    initial commit

--- .config/i3/wall-set.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-if [ -d ~/Pictures/bg ]; then
-	feh --bg-scale --no-fehbg ~/Picures/bg/$1
-fi
-if [ -d ~/.local/share/bg ]; then
-	feh --bg-scale --no-fehbg ~/.local/share/bg/$1
-fi
